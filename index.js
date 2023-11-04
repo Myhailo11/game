4e582fd959469cea1d9e819609dca4222ea258a8
Myhailo11 <myhailo.bielov@gmail.com>
2023-11-04 19:43:46 +0200

    04112023

---
+++ index.js
@@ -0,0 +1,19 @@
+const startBtn = document.querySelector(".start");
+const screens = document.querySelectorAll(".screen");
+const timeList = document.querySelector(".time__list");
+let time = 0;
+
+console.log("time:", time);
+console.log(timeList);
+startBtn.addEventListener("click", () => {
+  screens[0].classList.add("up");
+});
+
+timeList.addEventListener("click", (e) => {
+  if (e.target.classList.contains("time__btn")) {
+    time = parseInt(e.target.getAttribute("data-time"));
+    screens[1].classList.add("up");
+    console.log("time:", time);
+  }
+});
+
